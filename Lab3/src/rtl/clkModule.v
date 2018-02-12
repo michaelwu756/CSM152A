ef77d5b78fc5a96d03b5f4eab1e6ebd078933c50
Michael Wu <cheeserules43@gmail.com>
2018-02-12 14:55:05 -0800

    fixed clock timing

--- Lab3/src/rtl/clkModule.v
+++ Lab3/src/rtl/clkModule.v
@@ -2,27 +2,27 @@ module clkModule(clk,pause,clk_1, clk_2, clk_2p5, clk_500);
   input clk, pause;
   output clk_1, clk_2, clk_2p5, clk_500;
   
-  parameter TARGET_2 = 500/2;
-  parameter TARGET_2p5 = 1000/5;
-  parameter TARGET_1 = 500/1;
+  parameter TARGET_2 = 250;
+  parameter TARGET_2p5 = 200;
+  parameter TARGET_1 = 500;
   
   // Use 100Hz clock as clock divider
   clk500 secondaryClk(clk, pause, clk_500);
-  reg [5:0] counter2 = 0;
-  reg [5:0] counter2p5 = 0;
-  reg [6:0] counter1 = 0;
+  reg [5:0] counter2 = 1;
+  reg [5:0] counter2p5 = 1;
+  reg [6:0] counter1 = 1;
   
   // Update/reset counters for other clks
   always @(posedge clk_500)
   begin
-    counter2 = (counter2 == TARGET_2) ? 0 : counter2+1;
-    counter2p5 = (counter2p5 == TARGET_2p5) ? 0 : counter2p5+1;
-    counter1 = (counter1 == TARGET_1) ? 0 : counter1+1;
+    counter2 = (counter2 == TARGET_2) ? 1 : counter2+1;
+    counter2p5 = (counter2p5 == TARGET_2p5) ? 1 : counter2p5+1;
+    counter1 = (counter1 == TARGET_1) ? 1 : counter1+1;
   end
   
-  assign clk_2 = (counter2 == TARGET_2);
-  assign clk_2p5 = (counter2p5 == TARGET_2p5);
-  assign clk_1 = (counter1 == TARGET_1);
+  assign clk_2 = (counter2 == TARGET_2) ? 1 : 0;
+  assign clk_2p5 = (counter2p5 == TARGET_2p5) ? 1 : 0;
+  assign clk_1 = (counter1 == TARGET_1) ? 1 : 0;
 
 endmodule
 
@@ -32,16 +32,16 @@ module clk500(clk, pause, clk_500);
   
   parameter MASTER_FREQ = 28'd100000000;
   parameter TARGET_500 = MASTER_FREQ/500;
-  
+ 
   reg [27:0] counter500 = 0;
   
   always @(posedge clk)
   begin
     if(!pause) begin
       if(counter500 == TARGET_500)
-  		counter500 = 28'd0;
-  	else
-  		counter500 = counter500+1;
+        counter500 = 28'd0;
+     else
+        counter500 = counter500+1;
     end
   end
   
---
+++ Lab3/src/tb/test.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+module test;
+
+	// Inputs
+	reg clk;
+	reg pause;
+
+	// Outputs
+	wire clk_1;
+	wire clk_2;
+	wire clk_2p5;
+	wire clk_500;
+
+	// Instantiate the Unit Under Test (UUT)
+	clkModule uut (
+		.clk(clk),
+		.pause(pause),
+		.clk_1(clk_1),
+		.clk_2(clk_2),
+		.clk_2p5(clk_2p5),
+		.clk_500(clk_500)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		pause = 0;
+
+		// Wait 100 ns for global reset to finish
+		#5;
+      while (1) begin
+			clk=~clk;
+			#5;
+		end
+		// Add stimulus here
+
+	end
+
+endmodule
+
