665c873d3a64a5bd3e1024c92d23cf012ecbff90
Michael Wu <cheeserules43@gmail.com>
2018-03-11 21:49:48 -0700

    set velocity to zero at top of screen

--- Lab4/src/nerp/birdMovement.v
+++ Lab4/src/nerp/birdMovement.v
@@ -18,8 +18,11 @@ always @(posedge gameClk or posedge reset)
          end
       else if (!finished)
          begin
-            v_out<=(button) ? MAX_VELOCITY : (v_out-ACCEL > -MAX_VELOCITY) ? v_out-ACCEL : -MAX_VELOCITY;
-            y_out<=(y_out+v_out>SCREEN_HEIGHT-bird_height) ? SCREEN_HEIGHT-bird_height : (y_out+v_out<0) ? 0 : y_out+v_out;
+            v_out<=(y_out+v_out>SCREEN_HEIGHT-bird_height) ? 0 :
+               (button) ? MAX_VELOCITY :
+               (v_out-ACCEL > -MAX_VELOCITY) ? v_out-ACCEL : -MAX_VELOCITY;
+            y_out<=(y_out+v_out>SCREEN_HEIGHT-bird_height) ? SCREEN_HEIGHT-bird_height :
+               (y_out+v_out<bird_height) ? bird_height : y_out+v_out;
          end
    end
 
