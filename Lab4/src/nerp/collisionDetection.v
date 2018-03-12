82871cbd35697cbefffcd7ba6c6f5cbfc9b0fa80
Michael Wu <cheeserules43@gmail.com>
2018-03-12 16:06:03 -0700

    changed constants in collision detection

--- Lab4/src/nerp/collisionDetection.v
+++ Lab4/src/nerp/collisionDetection.v
@@ -11,22 +11,22 @@ module collisionDetection(
   wire [9:0] Atop, Abottom, Btop, Bbottom;
   reg hitColumn;
 
-  assign Atop = Ay + GAP_HEIGHT/2;
-  assign Abottom = Ay - GAP_HEIGHT/2;
-  assign Btop = By + GAP_HEIGHT/2;
-  assign Bbottom = By - GAP_HEIGHT/2;
-  assign Aleft = Ax - COL_WIDTH/2;
-  assign Bleft = Bx - COL_WIDTH/2;
-  assign Aright = Ax + COL_WIDTH/2;
-  assign Bright = Bx + COL_WIDTH/2;
+  assign Atop = Ay + pipe_height_gap;
+  assign Abottom = Ay - pipe_height_gap;
+  assign Btop = By + pipe_height_gap;
+  assign Bbottom = By - pipe_height_gap;
+  assign Aleft = Ax - pipe_width;
+  assign Bleft = Bx - pipe_width;
+  assign Aright = Ax + pipe_width;
+  assign Bright = Bx + pipe_width;
 
   always @(posedge gameClk) begin
 
-    if ((bird_x-bird_width/2 > Aleft && bird_x-bird_width/2 < Aright) || (bird_x+bird_width/2 > Aleft && bird_x+bird_width/2 < Aright)) begin
+    if ((bird_x-bird_width > Aleft && bird_x-bird_width < Aright) || (bird_x+bird_width > Aleft && bird_x+bird_width < Aright)) begin
       if (y_in+bird_height > Atop || y_in-bird_height < Abottom)
         hitColumn <= 1;
     end
-    else if ((bird_x-bird_width/2 > Bleft && bird_x-bird_width/2 < Bright) || (bird_x+bird_width/2 > Bleft && bird_x+bird_width/2 < Bright)) begin
+    else if ((bird_x-bird_width > Bleft && bird_x-bird_width < Bright) || (bird_x+bird_width > Bleft && bird_x+bird_width < Bright)) begin
       if (y_in+bird_height > Btop || y_in-bird_height < Bbottom)
         hitColumn <= 1;
     end
